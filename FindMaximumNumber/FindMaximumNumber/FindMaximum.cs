07d57b32d0d65e6c2d6982946eca3e92d92075ae
jayesh <jayeshborse0705@gmail.com>
2022-09-12 06:53:19 -0700

    [jayesh] to check maxnum in int

---
+++ FindMaximumNumber/FindMaximumNumber/FindMaximum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+     public class FindMaximum
+    {
+         public int FindMaxIntNumber(int firstnum, int secondnum, int thirdnum)
+         {
+             if (firstnum.CompareTo(secondnum) > 0 && firstnum.CompareTo(secondnum) > 0)
+                 return firstnum;
+             if (secondnum.CompareTo(firstnum) > 0 && secondnum.CompareTo(thirdnum) > 0)
+                 return secondnum;
+             else
+                 return thirdnum;
+
+         }
+    }
+}
---
+++ FindMaximumNumber/FindMaximumNumberTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindMaximumNumber;
+
+namespace FindMaximumNumberTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMaxFirstNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            int num = max.FindMaxIntNumber(1,3,2);
+            Assert.AreEqual(num, 7);
+        }
+    }
+}
